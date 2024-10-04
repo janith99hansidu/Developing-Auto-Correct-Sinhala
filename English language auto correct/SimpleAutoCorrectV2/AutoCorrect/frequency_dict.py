ba7790b5298d2988e17340c595488ab5315e1747
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-10-04 23:05:05 +0530

    Change Autocorrect class to function
    no need a class for load_freq_dict_from_file function

--- English language auto correct/SimpleAutoCorrectV2/AutoCorrect/frequency_dict.py
+++ English language auto correct/SimpleAutoCorrectV2/AutoCorrect/frequency_dict.py
@@ -5,6 +5,19 @@ import os
 import re
 from collections import Counter
 
+"""
+---sample usage---
+
+# where the text corpus is located
+text_corpus_file_path = "../data/eng_news_2005_10K-sentences.txt"
+
+# where the frequency dictionary file is located
+frequency_dict_file_path = "../data/output.json"
+
+# load frequency dict from file or create a new one if not found
+frequency_dict = load_freq_dict_from_file(frequency_dict_file_path, text_corpus_file_path)
+"""
+
 
 def load_corpus(corpus_file_path):
     """ in each line in the text corpus removes the leading numbers
@@ -13,54 +26,32 @@ def load_corpus(corpus_file_path):
 
     with open(corpus_file_path, 'r', encoding='utf-8') as file:
         for line in file:
-            # split the sentence in to words
+            # split the sentence into words
             words = line.split()
-            # clean the words remove any punctuations and non word charters
+            # clean the words remove any punctuations and non-word characters
             cleaned_words = [re.sub(r'^[\W\d]+|[\W\d]+$', '', word) for word in words]
             # remove all the empty string from the word
             cleaned_words = [word.lower() for word in cleaned_words if word]
             # add the words to dict
             word_counter.update(cleaned_words)
 
-        return dict(word_counter)
+    return dict(word_counter)
 
 
-class FrequencyDict:
-    def __init__(self, freq_dict_file_path, text_corpus_file_path):
-        self.freq_dict_file_path = freq_dict_file_path
-        self.text_corpus_file_path = text_corpus_file_path
+def load_freq_dict_from_file(freq_dict_file_path, text_corpus_file_path):
+    """if output file exists in the directory load frequency dict from it
+        else build the frequency dict"""
+    if os.path.exists(freq_dict_file_path):
+        with open(freq_dict_file_path, 'r', encoding='utf-8') as f:
+            # convert loaded dict values back to int
+            frequency_dict = {word: int(count) for word, count in json.load(f).items()}
 
-    def load_freq_dict_from_file(self):
-        """if output file is exist in the directory load frequency dict from it
-            else build the frequency dict"""
-        if os.path.exists(self.freq_dict_file_path):
-            with open(self.freq_dict_file_path, 'r', encoding='utf-8') as f:
-                # convert loaded dict values back to int
-                frequency_dict = {word: int(count) for word, count in json.load(f).items()}
+        return frequency_dict
+    else:
+        # if the frequency_dict_file is not in the path make a dict
+        frequency_dict = load_corpus(text_corpus_file_path)
+        # and save the corpus into the output.json
+        with open(freq_dict_file_path, 'w', encoding='utf-8') as f:
+            json.dump(frequency_dict, f)
 
-            return frequency_dict
-        else:
-            # if the frequency_dict_file is not in the path make a dict
-            frequency_dict = load_corpus(self.text_corpus_file_path)
-            # and save the corpus in to the output.json
-            with open(self.freq_dict_file_path, 'w', encoding='utf-8') as f:
-                json.dump(frequency_dict, f)
-
-            return frequency_dict
-
-    """ ---sample usage---
-    
-    # where the text corpus is located
-    text_corpus_file_path = "../data/eng_news_2005_10K-sentences.txt"
-    
-    # where the frequency dictionary file is located 
-    frequency_dict_file_path = "../data/output.json"
-    
-    # make an object from frequency dict class
-    frequency_dict_obj = FrequencyDict(frequency_dict_file_path, text_corpus_file_path)
-    
-    # call the method load_freq_dict_from_file()
-    # returns a dictionary of frequencies of text corpus
-    frequency_dict_obj.load_freq_dict_from_file()
-    
-    """
+        return frequency_dict
