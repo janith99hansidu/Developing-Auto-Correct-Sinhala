9866ca1cd53bdb616c0931b62afa137089c52abb
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-10-05 18:09:15 +0530

    Set threshold value

--- English language auto correct/SimpleAutoCorrectV2/main.py
+++ English language auto correct/SimpleAutoCorrectV2/main.py
@@ -8,23 +8,28 @@ from AutoCorrect.typos import Word
 
 
 class Speller:
-    def __init__(self):
+    def __init__(self, threshold):
         self.frequency_dict = load_freq_dict_from_file("./data/output.json", "./data/eng_news_2005_10K-sentences.txt")
+        self.threshold = threshold  # return only most probable candidates
 
     def get_candidates(self, word):
         # if the word already in the frequency dict.keys() that means it is correct word no candidates
         # otherwise check for candidates
         if word in self.frequency_dict:
             return []
+
         return sorted([(candidate, self.frequency_dict[candidate])
                        for candidate in Word(word).candidates()
                        if candidate in self.frequency_dict],
                       key=lambda x: x[1],
-                      reverse=True)
+                      reverse=True)[:self.threshold]
 
 
 if __name__ == '__main__':
-    speller = Speller()
+    # set the threshold value to how many of most probable words that should return
+    threshold_value = 3
+    # create speller object to get candidates
+    speller = Speller(threshold_value)
     print('input a word: ')
     word = input().lower()
     print("possible corrections :", speller.get_candidates(word))
